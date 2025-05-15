77b520b76b5b4b3b133b471c0abb65603189a47d
Mahmoud Fawzy Taha-Elaraby <1998mft1998@gmail.com>
2025-05-15 23:36:17 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+createRoot(document.getElementById("root")!).render(<App />);
+<React.StrictMode>
+  <BrowserRouter basename="/mft1998-portfolio/">
+    <Routes>
+      <Route path="*" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+</React.StrictMode>
+
