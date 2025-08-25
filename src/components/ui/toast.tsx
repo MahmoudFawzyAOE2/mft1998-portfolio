cf769aa4e7c567f98177cc4ae0c0ff68a61b5b88
Mahmoud Fawzy Taha-Elaraby <1998mft1998@gmail.com>
2025-08-25 18:14:55 +0300

    add IDs to toast tags

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -13,6 +13,7 @@ const ToastViewport = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ToastPrimitives.Viewport
     ref={ref}
+    id="toast-viewport"
     className={cn(
       "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
       className
@@ -74,6 +75,7 @@ const ToastClose = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ToastPrimitives.Close
     ref={ref}
+    id ="toast-close"
     className={cn(
       "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
       className
@@ -92,6 +94,7 @@ const ToastTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ToastPrimitives.Title
     ref={ref}
+    id ="toast-title"
     className={cn("text-sm font-semibold", className)}
     {...props}
   />
@@ -104,6 +107,7 @@ const ToastDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ToastPrimitives.Description
     ref={ref}
+    id ="toast-description"
     className={cn("text-sm opacity-90", className)}
     {...props}
   />
